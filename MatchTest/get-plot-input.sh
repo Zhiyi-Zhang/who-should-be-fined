56a5918c0e2d9d49d6477122afc64bcacdc9cb73
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-05-12 15:35:56 -0700

    update missing part

--- MatchTest/get-plot-input.sh
+++ MatchTest/get-plot-input.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-rm -rf non-collusion-result
-rm -rf collusion-result
-
-mkdir non-collusion-result
-mkdir collusion-result
-
-
-
 Xarray=(0.01 0.015 0.02 0.025 0.03 0.04 0.05 0.1 0.5 0.8 1)
 # 3 total node, single node leaker
 Pattern3_single=(919 991)
@@ -24,94 +16,98 @@ Pattern6_three=(911909+900919 911099 910199+901199)
 Pattern6_six=(111000+100111 111111 111001)
 
 #######################################SINGLE LEAKER############################
-# create plot input file
-# for i in "${Pattern3_single[@]}"
-# do
-#   touch non-collusion-result/${i}
-# done
-# for i in "${Pattern6_single[@]}"
-# do
-#   touch non-collusion-result/${i}
-# done
-# for i in "${Pattern9_single[@]}"
-# do
-#   touch non-collusion-result/${i}
-# done
-# # fulfill
-# for i in "${Xarray[@]}"
-# do
-#   for j in "${Pattern3_single[@]}"
-#   do
-#     ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
-#   done
-#   for j in "${Pattern6_single[@]}"
-#   do
-#     ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
-#   done
-#   for j in "${Pattern9_single[@]}"
-#   do
-#     ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
-#   done
-# done
+rm -rf non-collusion-result
+mkdir non-collusion-result
 
-# #######################################COLLUSION LEAKER############################
-# # create plot input file
-# for i in "${Pattern6_two[@]}"
-# do
-#   touch collusion-result/${i}
-# done
-# for i in "${Pattern6_three[@]}"
-# do
-#   touch collusion-result/${i}
-# done
-# for i in "${Pattern6_six[@]}"
-# do
-#   touch collusion-result/${i}
-# done
-# for i in "${Xarray[@]}"
-# do
-#   for j in "${Pattern6_two[@]}"
-#   do
-#     ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
-#   done
-
-#   for j in "${Pattern6_three[@]}"
-#   do
-#     ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
-#   done
-
-#   for j in "${Pattern6_six[@]}"
-#   do
-#     ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
-#   done
-# done
-
-# #######################################DIFF OT############################
-rm -rf different-ot-result
-mkdir different-ot-result
-
-OTs=(1-2 2-3 3-4)
-
-# create plot input file
+for i in "${Pattern3_single[@]}"
+do
+  touch non-collusion-result/${i}
+done
 for i in "${Pattern6_single[@]}"
 do
-  touch different-ot-result/${i}
+  touch non-collusion-result/${i}
+done
+for i in "${Pattern9_single[@]}"
+do
+  touch non-collusion-result/${i}
+done
+# fulfill
+for i in "${Xarray[@]}"
+do
+  for j in "${Pattern3_single[@]}"
+  do
+    ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
+  done
+  for j in "${Pattern6_single[@]}"
+  do
+    ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
+  done
+  for j in "${Pattern9_single[@]}"
+  do
+    ./test.py non-collusion/${j}-${i}.txt ${i} >> non-collusion-result/${j}
+  done
+done
+
+# #######################################COLLUSION LEAKER############################
+rm -rf collusion-result
+mkdir collusion-result
+
+for i in "${Pattern6_two[@]}"
+do
+  touch collusion-result/${i}
 done
 for i in "${Pattern6_three[@]}"
 do
-  touch different-ot-result/${i}
+  touch collusion-result/${i}
 done
-for i in "${OTs[@]}"
+for i in "${Pattern6_six[@]}"
 do
-  for j in "${Pattern6_single[@]}"
+  touch collusion-result/${i}
+done
+for i in "${Xarray[@]}"
+do
+  for j in "${Pattern6_two[@]}"
   do
-    ./test.py different-ot/${j}-${i}-0.9.txt ${i} >> different-ot-result/${j}
+    ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
   done
+
   for j in "${Pattern6_three[@]}"
   do
-    ./test.py different-ot/${j}-${i}-0.9.txt ${i} >> different-ot-result/${j}
+    ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
+  done
+
+  for j in "${Pattern6_six[@]}"
+  do
+    ./test.py collusion/${j}-${i}.txt ${i} >> collusion-result/${j}
   done
 done
+
+# #######################################DIFF OT############################
+# rm -rf different-ot-result
+# mkdir different-ot-result
+
+# OTs=(1-2 2-3 3-4)
+
+# # create plot input file
+# for i in "${Pattern6_single[@]}"
+# do
+#   touch different-ot-result/${i}
+# done
+# for i in "${Pattern6_three[@]}"
+# do
+#   touch different-ot-result/${i}
+# done
+# for i in "${OTs[@]}"
+# do
+#   for j in "${Pattern6_single[@]}"
+#   do
+#     ./test.py different-ot/${j}-${i}-0.9.txt ${i} >> different-ot-result/${j}
+#   done
+#   for j in "${Pattern6_three[@]}"
+#   do
+#     ./test.py different-ot/${j}-${i}-0.9.txt ${i} >> different-ot-result/${j}
+#   done
+# done
 
 #######################################DIFF ALLOC############################
 # rm -rf different-alloc-result
