c86240c4b2bc667683119fafcc5de5b8d994ed15
shanxigy <shanxigy@pku.edu.cn>
2019-05-11 22:42:47 -0700

    non_collude_finished

---
+++ MatchTest/non-collusion-result/non_collusion_all.m
@@ -0,0 +1,25 @@
+function non_collusion_all()
+clear all;
+close all;
+non_collusion_result('919');
+
+clear all;
+close all;
+non_collusion_result('991');
+
+clear all;
+close all;
+non_collusion_result('919999');
+
+clear all;
+close all;
+non_collusion_result('999991');
+
+clear all;
+close all;
+non_collusion_result('999199999');
+
+clear all;
+close all;
+non_collusion_result('999991999');
+end
