7603ae9ac9bf809cec38016db9cfad8ef2f36649
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-05-13 16:07:08 -0700

    really finished this time

--- MatchTest/generate-input-alloc.sh
+++ MatchTest/generate-input-alloc.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
 
-# 6 total node, 2 node collusion
-Pattern6_two=(911999+901999 991199 999119)
+
+Xarray=(0.01 0.015 0.02 0.025 0.03 0.04 0.05 0.1 0.5 0.8 1)
 # 6 total node, 3 node collusion
 Pattern6_three=(911909+901919 911099 910199+901199)
-# 6 total node, 6 node collusion
-Pattern6_six=(111000+100111 111111 110000)
 
 Alloc=(0 100 300 500 1000 2000)
 TotalObjectNum=20000
-
-for j in "${Pattern6_two[@]}"
-do
-    for k in "${Alloc[@]}"
-    do
-        ./input-gen-alloc.py different-alloc/${j}-${k}.txt 6 ${TotalObjectNum} ${j} 0.1 ${k}
-    done
-done
 
 for j in "${Pattern6_three[@]}"
 do
     for k in "${Alloc[@]}"
     do
-        ./input-gen-alloc.py different-alloc/${j}-${k}.txt 6 ${TotalObjectNum} ${j} 0.1 ${k}
-    done
-done
-
-for j in "${Pattern6_six[@]}"
-do
-    for k in "${Alloc[@]}"
-    do
-        ./input-gen-alloc.py different-alloc/${j}-${k}.txt 6 ${TotalObjectNum} ${j} 0.1 ${k}
+        for i in "${Xarray[@]}"
+        do
+            ./input-gen-alloc.py different-alloc/${j}-${k}-${i}.txt 6 ${TotalObjectNum} ${j} ${i} ${k}
+        done
     done
 done
