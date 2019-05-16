10bd1ded7881b24dc87602cac57dcda472eef61b
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-05-15 17:33:33 -0700

    update final input

--- MatchTest/get-plot-input-sender.sh
+++ MatchTest/get-plot-input-sender.sh
@@ -2,34 +2,29 @@
 
 Xarray=(0.01 0.015 0.02 0.025 0.03 0.04 0.05 0.1 0.5 0.8 1)
 
-rm sender-leakage-result/sender-ot.txt
-touch sender-leakage-result/sender-ot.txt
-for i in "${Xarray[@]}"
-do
-  ./sender-leakage-input-gen.py 6 20000 0 1-2 ${i} >> sender-leakage-result/sender-ot.txt
-done
-for i in "${Xarray[@]}"
-do
-  ./sender-leakage-input-gen.py 6 20000 0 2-3 ${i} >> sender-leakage-result/sender-ot.txt
-done
-for i in "${Xarray[@]}"
-do
-  ./sender-leakage-input-gen.py 6 20000 0 3-4 ${i} >> sender-leakage-result/sender-ot.txt
-done
+# rm sender-leakage-result/sender-ot.txt
+# touch sender-leakage-result/sender-ot.txt
+# for i in "${Xarray[@]}"
+# do
+#   ./sender-leakage-input-gen.py 6 20000 0 1-2 ${i} >> sender-leakage-result/sender-ot.txt
+# done
+# for i in "${Xarray[@]}"
+# do
+#   ./sender-leakage-input-gen.py 6 20000 0 2-3 ${i} >> sender-leakage-result/sender-ot.txt
+# done
+# for i in "${Xarray[@]}"
+# do
+#   ./sender-leakage-input-gen.py 6 20000 0 3-4 ${i} >> sender-leakage-result/sender-ot.txt
+# done
 
-Alloc=(0 100 300 500 1000 2000)
+Alloc=(0 100 300 500 1000)
 
 rm sender-leakage-result/sender-alloc.txt
 touch sender-leakage-result/sender-alloc.txt
 for i in "${Alloc[@]}"
 do
-  ./sender-leakage-input-gen.py 6 20000 ${i} 1-2 0.1 >> sender-leakage-result/sender-alloc.txt
-done
-for i in "${Alloc[@]}"
-do
-  ./sender-leakage-input-gen.py 6 20000 ${i} 2-3 0.1 >> sender-leakage-result/sender-alloc.txt
-done
-for i in "${Alloc[@]}"
-do
-  ./sender-leakage-input-gen.py 6 20000 ${i} 3-4 0.1 >> sender-leakage-result/sender-alloc.txt
+  for j in "${Xarray[@]}"
+  do
+    ./sender-leakage-input-gen.py 6 20000 ${i} 1-2 ${j} >> sender-leakage-result/sender-alloc-1-2.txt
+  done
 done
--- MatchTest/get-plot-input-single-leaker.sh
+++ MatchTest/get-plot-input-single-leaker.sh
@@ -4,15 +4,26 @@ Xarray=(0.01 0.015 0.02 0.025 0.03 0.04 0.05 0.1 0.5 0.8 1)
 
 # rm non-collusion-result/single-leaker-ot.txt
 # touch non-collusion-result/single-leaker-ot.txt
-for i in "${Xarray[@]}"
+# for i in "${Xarray[@]}"
+# do
+#   ./single-leaker-input-gen.py 6 20000 0 1-2 ${i} >> non-collusion-result/single-leaker-ot.txt
+# done
+# for i in "${Xarray[@]}"
+# do
+#   ./single-leaker-input-gen.py 6 20000 0 2-3 ${i} >> non-collusion-result/single-leaker-ot.txt
+# done
+# for i in "${Xarray[@]}"
+# do
+#   ./single-leaker-input-gen.py 6 20000 0 3-4 ${i} >> non-collusion-result/single-leaker-ot.txt
+# done
+
+Alloc=(0 100 300 500 1000)
+
+for j in "${Alloc[@]}"
 do
-  ./single-leaker-input-gen.py 6 20000 0 1-2 ${i} >> non-collusion-result/single-leaker-ot.txt
+  for i in "${Xarray[@]}"
+  do
+    ./single-leaker-input-gen.py 6 20000 ${j} 1-2 ${i} >> non-collusion-result/single-leaker-alloc-1-2.txt
+  done
 done
-for i in "${Xarray[@]}"
-do
-  ./single-leaker-input-gen.py 6 20000 0 2-3 ${i} >> non-collusion-result/single-leaker-ot.txt
-done
-for i in "${Xarray[@]}"
-do
-  ./single-leaker-input-gen.py 6 20000 0 3-4 ${i} >> non-collusion-result/single-leaker-ot.txt
-done
+
